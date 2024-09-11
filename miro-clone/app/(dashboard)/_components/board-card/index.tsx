d043b2f272f3c46e3860a62bb690525756e07029
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-11 12:10:11 +0300

    Board Cards in work

---
+++ miro-clone/app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,45 @@
+"use client"; 
+
+import Link from "next/link";
+import Image from "next/image";
+import { Overlay } from "./overlay";
+
+interface BoardCardProps { 
+    id: string; 
+    title: string; 
+    authorName: string; 
+    authorId: string; 
+    createdAt: number; 
+    imageUrl: string;
+    orgId: string; 
+    isFavourite: boolean; 
+}
+
+export const BoardCard = ({
+    id, 
+    title, 
+    authorName, 
+    authorId, 
+    createdAt, 
+    imageUrl, 
+    orgId, 
+    isFavourite
+}: BoardCardProps) => { 
+    return ( 
+        <div>
+            <Link href = { `/board/${id}`}>
+                <div className = "group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+                    <div className = "relative flex-1 bg-amber-50">
+                        <Image
+                            src = { imageUrl }
+                            alt = { title }
+                            className = "object-fit"
+                            fill
+                        /> 
+                        <Overlay /> 
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
--- miro-clone/app/(dashboard)/_components/board-list.tsx
+++ miro-clone/app/(dashboard)/_components/board-list.tsx
@@ -3,6 +3,9 @@
 import { EmptySearch } from "./empty-search";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptyBoards } from "./empty-boards";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
 
 interface BoardListProps { 
     orgId: string; 
@@ -13,7 +16,15 @@ interface BoardListProps {
 }; 
 
 export const BoardList = ( { orgId, query }: BoardListProps) => { 
-    const data = [] // TODO - API DATA 
+    const data = useQuery(api.boards.get, { orgId }); 
+
+    if(data == undefined) { 
+        return ( 
+            <div>
+                Loading...
+            </div>
+        )
+    }
 
     if(!data?.length && query.search) { 
         return <EmptySearch />;
@@ -29,7 +40,26 @@ export const BoardList = ( { orgId, query }: BoardListProps) => {
     }
     return ( 
         <div>
-            { JSON.stringify(query) }
+            <h2 className = "text-3xl font-semibold">
+                { query.favourites ? "Favourite Boars" : "Team Boards" }
+            </h2>
+            <div className = "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                { data?.map((board) => { 
+                    return ( 
+                        <BoardCard 
+                            key = { board._id }
+                            id = { board._id }
+                            title = { board.title }
+                            imageUrl = { board.imageUrl }
+                            authorId = { board.authorId }
+                            authorName = { board.authorName }
+                            createdAt = { board._creationTime }
+                            orgId = { board.orgId }
+                            isFavourite = { false }
+                        /> 
+                    )
+                })}
+            </div>
         </div>
     )
 }
