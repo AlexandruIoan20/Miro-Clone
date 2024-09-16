13d24bb3fbb685c98c052ae83ba06fec1d471a69
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-16 22:40:58 +0300

    Room participants

--- miro-clone/app/board/[boardId]/_components/participants.tsx
+++ miro-clone/app/board/[boardId]/_components/participants.tsx
@@ -2,13 +2,16 @@
 
 import { useOthers, useSelf } from "@liveblocks/react/suspense";
 import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
 
-const MAX_SHOWN_USERS = -1; 
+const MAX_SHOWN_USERS = 2; 
 
 export const Participants = () => { 
     const users = useOthers(); 
     const currentUser = useSelf(); 
     const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
+    console.log({ users }); 
 
     return ( 
         <div className = "absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
@@ -16,6 +19,7 @@ export const Participants = () => {
                 { users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => { 
                     return ( 
                         <UserAvatar
+                            borderColor = { connectionIdToColor(connectionId) }
                             key = { connectionId }
                             src = { info?.picture }
                             name = { info?.name }
@@ -27,6 +31,7 @@ export const Participants = () => {
                 { currentUser && 
                     ( 
                         <UserAvatar 
+                            borderColor = { connectionIdToColor(currentUser.connectionId) }
                             src = { currentUser.info?.picture }
                             name = { `${currentUser.info?.name} (You)`}
                             fallback = { currentUser.info.name?.[0] }
