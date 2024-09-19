6a8fec7b1aa36e0c2598bcdde1aaa3bb9790bcc3
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-19 23:41:59 +0300

    Other hooks and improvements

--- miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -3,7 +3,7 @@
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState, useEffect } from "react";
 import { Camera, CanvasMode, CanvasState, Color, LayerType, Point, Layer, Side, XYWH} from "@/types/canvas";
 import { useHistory, useCanUndo, useCanRedo, useMutation, useStorage, useOthersMapped, useSelf } from "@liveblocks/react/suspense";
 import { CursorsPresence } from "./cursors-presence";
@@ -14,6 +14,8 @@ import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
 import { SelectionTools } from "./selection-tools";
 import { Path } from "./path";
+import { useDisableScrollBounce } from "@/hooks/use-disable-scroll-bounce";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
 
 const MAX_LAYERS = 100;
 
@@ -330,6 +332,34 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
         return layerIdsToColorSelection; 
     }, [ selections ]); 
+
+    const deleteLayers = useDeleteLayers(); 
+
+    useDisableScrollBounce(); 
+
+    useEffect( () => {
+        function onKeyDown(e: KeyboardEvent) { 
+            switch(e.key) { 
+                case "z":
+                    if(e.ctrlKey || e.metaKey) { 
+                        if(e.shiftKey) { 
+                            history.redo(); 
+                        } else { 
+                            history.undo(); 
+                        }
+                    }
+
+                    break;
+            }
+        }
+
+
+        document.addEventListener("keydown", onKeyDown);
+        
+        return () => { 
+            document.removeEventListener("keydown", onKeyDown)
+        }
+    }, [ deleteLayers, history ]); 
 
     return ( 
         <main className = "h-full w-full relative bg-neutral-100 touch-none">
