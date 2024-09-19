352beeeb75c32f93e9d4437bc631735e691e1ee8
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-19 13:33:03 +0300

    Color change for layers

--- miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -12,6 +12,7 @@ import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -217,7 +218,10 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                 undo = { history.undo }
                 redo = { history.redo }
             /> 
-
+            <SelectionTools
+                camera = { camera }
+                setLastUsedColor = { setLastUsedColor }
+            /> 
             <svg 
                 onWheel = { onWheel }
                 onPointerMove = { onPointerMove }
