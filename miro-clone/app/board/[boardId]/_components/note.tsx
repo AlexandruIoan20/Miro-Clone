434dda7d45528e450c4031f0db4e3c7acf0b2f44
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-19 20:51:05 +0300

    Sticky Note Element

---
+++ miro-clone/app/board/[boardId]/_components/note.tsx
@@ -0,0 +1,81 @@
+"use client"; 
+
+import { Kalam } from "next/font/google";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable"; 
+
+import { cn, colorToCss, getContrastingTextColor } from "@/lib/utils";
+import { NoteLayer } from "@/types/canvas";
+import { useMutation } from "@liveblocks/react/suspense";
+
+const font = Kalam({ 
+    subsets: ["latin"], 
+    weight: [ "400" ]
+}); 
+
+const calculateFontSize = (width: number, height: number) => { 
+    const MAX_FONT_SIZE = 96; 
+    const SCALE_FACTOR = 0.15; 
+
+    const fontSizeBasedOnHeight = height * SCALE_FACTOR; 
+    const fontSizeBasedOnWidth = width * SCALE_FACTOR; 
+
+    return Math.min(
+        fontSizeBasedOnHeight, fontSizeBasedOnWidth, MAX_FONT_SIZE
+    ); 
+}; 
+
+interface NoteProps { 
+    id: string,
+    layer: NoteLayer, 
+    onPointerDown: (e: React.PointerEvent, id: string) => void,
+    selectionColor?: string, 
+}; 
+
+export const Note = ({
+    id, 
+    layer, 
+    onPointerDown,
+    selectionColor
+}: NoteProps) => { 
+    const { x, y, width, height, fill, value } = layer; 
+
+    const updateValue = useMutation((
+        { storage }, 
+        newValue: string
+    ) => {
+        const liveLayers = storage.get("layers"); 
+        liveLayers.get(id)?.set("value", newValue); 
+    }, []); 
+
+    const handleContentChange = (e: ContentEditableEvent) => { 
+        updateValue(e.target.value); 
+    }; 
+
+    return ( 
+        <foreignObject
+            x = { x }
+            y = { y }
+            width = { width }
+            height = { height }
+            onPointerDown = { (e) => onPointerDown(e, id) }
+            style = {{
+                outline: selectionColor ? `1px solid ${selectionColor}` : "none", 
+            }}
+            className = "shadow-md drop-shadow-xl"
+        >
+            <ContentEditable
+                html = { value || "Text" }
+                onChange = { handleContentChange }
+                className = { cn(
+                    "h-full w-full flex items-center justify-center text-center outline-none", 
+                    font.className 
+                )}
+                style = {{
+                    color: fill ? getContrastingTextColor(fill): "#000", 
+                    fontSize: calculateFontSize(width, height), 
+                    backgroundColor: fill ? colorToCss(fill) : "#000", 
+                }}
+            /> 
+        </foreignObject>
+    )
+}   
