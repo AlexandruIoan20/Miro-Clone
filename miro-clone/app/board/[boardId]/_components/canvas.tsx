363634e06ec42550174835812c045be500443025
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-09-19 12:40:24 +0300

    Translate Layers

--- miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -61,6 +61,39 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         setCanvasState( { mode: CanvasMode.None }); 
     }, [ lastUsedColor ]); 
 
+    const translateSelectedLayers = useMutation( ({ storage, self }, point: Point) => {
+        if(canvasState.mode !== CanvasMode.Translating) { 
+            return; 
+        }
+
+        const offset = { 
+            x: point.x - canvasState.current.x, 
+            y: point.y - canvasState.current.y, 
+        }
+
+        const liveLayers = storage.get("layers"); 
+        for(const id of self.presence.selection) { 
+            const layer = liveLayers.get(id); 
+            if(layer) { 
+                layer.update({
+                    x: layer.get("x") + offset.x, 
+                    y: layer.get("y") + offset.y, 
+                })
+            }
+        } 
+
+        setCanvasState({ 
+            mode: CanvasMode.Translating,
+            current: point, 
+        })
+    }, [ canvasState ]); 
+
+    const unselectLayers = useMutation( ({ self, setMyPresence }) => {
+        if(self.presence.selection.length > 0) { 
+            setMyPresence({ selection: [] }, { addToHistory: true }); 
+        }
+    }, [])
+
     const resizeSelectedLayer = useMutation( ({ storage, self }, point: Point) => { 
         if(canvasState.mode !== CanvasMode.Resizing) { 
             return; 
@@ -96,11 +129,13 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
         const current = pointerEventToCanvasPoint(e, camera); 
 
-        if(canvasState.mode === CanvasMode.Resizing) { 
+        if(canvasState.mode === CanvasMode.Translating) { 
+            translateSelectedLayers(current); 
+        } else if(canvasState.mode === CanvasMode.Resizing) { 
             resizeSelectedLayer(current); 
         }
         setMyPresence({ cursor: current }); 
-    }, [ canvasState ]); 
+    }, [ canvasState, translateSelectedLayers ]); 
 
     const onPointerLeave = useMutation( ({ setMyPresence }) => { 
         setMyPresence({ cursor: null })
@@ -109,12 +144,14 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     const onPointerUp = useMutation( ({}, e) => {
         const point = pointerEventToCanvasPoint(e, camera); 
 
-        console.log({ 
-            point,
-            mode: canvasState.mode, 
-        })
-
-        if(canvasState.mode === CanvasMode.Inserting) { 
+        if(
+            canvasState.mode === CanvasMode.None || canvasState.mode === CanvasMode.Pressing
+        ) { 
+            unselectLayers(); 
+            setCanvasState({ 
+                mode: CanvasMode.None, 
+            })
+        } else if(canvasState.mode === CanvasMode.Inserting) { 
             insertLayer(canvasState.layerType, point); 
         } else { 
             setCanvasState({ 
@@ -123,7 +160,19 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         };
 
         history.resume();
-    }, [ camera, canvasState, history, insertLayer ]); 
+    }, [ camera, canvasState, history, insertLayer, unselectLayers ]); 
+
+    const onPointerDown = useCallback ( (e: React.PointerEvent) => {
+        const point = pointerEventToCanvasPoint(e, camera); 
+        if(canvasState.mode === CanvasMode.Inserting) { 
+            return;
+        }
+
+        //TODO: Add case for drawing
+        setCanvasState({ 
+            origin: point, mode: CanvasMode.Pressing
+        })
+    }, [ camera, canvasState.mode, setCanvasState ]); 
 
     const selections = useOthersMapped((other) => other.presence.selection); 
 
@@ -175,6 +224,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                 className = "h-[100vh] w-[100vw]"
                 onPointerLeave = { onPointerLeave }
                 onPointerUp = { onPointerUp }
+                onPointerDown = { onPointerDown }
                 >
                 <g
                     style = {{ 
